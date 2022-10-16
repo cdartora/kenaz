1ed00bde4ee45ed3582b83506c0f2eab45cfcfb2
Carlos Dartora <carlosdalsoler@gmail.com>
2022-10-16 01:00:49 -0300

    ci: :construction_worker: faz o teste passar

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import WrappedApp from "./App";
 describe("App", () => {
   it("renderiza Hello World", () => {
     render(<WrappedApp />);
-    expect(screen.getByText(/Not Home/i)).toBeInTheDocument();
+    expect(screen.getByText(/Home/i)).toBeInTheDocument();
   });
 });
