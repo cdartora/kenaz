14693d1e85432cbc848f2ae599f0cadf5b520172
Carlos Dartora <carlosdalsoler@gmail.com>
2022-10-28 17:57:45 -0300

    test(test): :bug: altera o teste do app para buscar pelo h1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import WrappedApp from "./App";
 describe("App", () => {
   it("renderiza Hello World", () => {
     render(<WrappedApp />);
-    expect(screen.getByText(/Home/i)).toBeInTheDocument();
+    expect(screen.getByRole("heading", { level: 1 })).toBeInTheDocument();
   });
 });
