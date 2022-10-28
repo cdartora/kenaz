81d2ab0a115b597ab006634c10ef5b8751db097a
Carlos Dartora <carlosdalsoler@gmail.com>
2022-10-28 17:53:20 -0300

    feat(header): :sparkles: implementa e estiliza o header mobile

---
+++ src/components/Header/sidebarData.ts
@@ -0,0 +1,27 @@
+export type SidebarDataType = {
+  title: string;
+  path: string;
+};
+
+export const SidebarData: SidebarDataType[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Livros",
+    path: "/livros",
+  },
+  {
+    title: "Eventos",
+    path: "/eventos",
+  },
+  {
+    title: "Cardápio",
+    path: "/cardapio",
+  },
+  {
+    title: "Sobre Nós",
+    path: "/sobre",
+  },
+];
